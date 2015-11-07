1b248c99dcf783977a752a1d0f501d823a453c1f
sanjaysagarp <sanjaysagarp@hotmail.com>
2015-11-06 21:25:32 -0800

    quiz view controller fixed. Need to add questions

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+struct question {
+    //Each question has a questionText, possibleAnswers, answer
+}
 class ViewController: UIViewController {
     var mathQuestions: [String:[String]] = [
         "4 + 7 = " : ["2","3","11","9"]
@@ -22,18 +24,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var answer4: UIButton!
     @IBOutlet weak var submit: UIButton!
     
+    lazy var buttons: [UIButton] = [self.answer1, self.answer2, self.answer3, self.answer4]
+    var userAnswer = ""
     @IBAction func answerButton(sender: UIButton) {
-        submit.alpha = 1;
-        sender.layer.borderWidth = 4
-        sender.layer.borderColor = UIColor.blackColor().CGColor
-        sender.selected = true
+            for button in self.buttons {
+                button.selected = false
+                button.layer.borderWidth = 0
+            }
+            submit.alpha = 1;
+            submit.enabled = true
+            sender.layer.borderWidth = 4
+            sender.layer.borderColor = UIColor.blackColor().CGColor
+            sender.selected = true
+            userAnswer = sender.currentTitle!
     }
     @IBAction func submitButton(sender: UIButton) {
+        
+        for button in self.buttons {
+            if button.currentTitle == "11" { //answer check
+                if button.selected {
+                    button.layer.backgroundColor = UIColor.greenColor().CGColor
+                } else {
+                    button.layer.borderWidth = 4
+                    button.layer.borderColor = UIColor.redColor().CGColor
+                }
+            }
+            button.enabled = false
+        }
+        sender.enabled = false
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        submit.enabled = false
         questionText.text! = "4 + 7 = "
         answer1.setTitle("2", forState: .Normal)
         answer2.setTitle("3", forState: .Normal)
